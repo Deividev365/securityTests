eb840357a39fa8404aaeac986cb60ec720a06dd3
Deivid Almeida <deividalmeida365@gmail.com>
2021-10-17 20:20:30 -0300

    new changes

--- keylogger/keyLogger.py
+++ keylogger/keyLogger.py
@@ -1,10 +1,2 @@
-# Define `plus()`
-def plus(a,b):
-  sum = a + b
-  return (sum, a)
+print("Hello world!!")
 
-# Call `plus()` and unpack variables 
-sum, a = plus(3,4)
-
-# Print `sum()`
-print(sum)
